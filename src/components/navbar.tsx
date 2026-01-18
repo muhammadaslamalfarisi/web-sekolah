51a30df416e5dc379158aef9e5cfa514ef437bfc
muhammadaslamalfarisi <aslamalfarisi0012@gmail.com>
2026-01-18 23:13:01 +0800

    update layout navbar dan logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,24 @@
 import { IconInnerShadowTop } from "@tabler/icons-react";
 import { NavMenu } from "./nav-menu";
 
+import LogoSekolah from "../../public/logo-sekolah.png";
+import Image from "next/image";
+
 export default function Navbar() {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 z-50">
       <div className="flex items-center justify-between h-32">
-        <div className="size-20">
-          <img
-            src="https://lh3.googleusercontent.com/sitesv/AAzXCkcAMCyAY-P2U8L_8Ze4bjwTtLY-lp2Om95DjOwUbe92xMt2xszXTlx6WgyverSeUlkZ9d5spU7V9Hhcx3gZhugt9YLsNGm5y0NlJVYeLXK26hcg5oo2p2ryLemHHrB6deNooauBnOxGsjaHXTiBLHj6U-7g6jGJMmJjgEVAD4mCuvRbifn79WOukeU=w16383"
-            alt=""
-          />
+        <div className="flex items-center">
+          <div className="flex items-center gap-3">
+            <Image
+              src={LogoSekolah}
+              alt="Logo SD Negeri 1 Batu Rakit"
+              className="w-10 h-10 object-contain"
+            />
+            <span className="text-xl font-medium">SD Negeri 1 Batu Rakit</span>
+          </div>
         </div>
+
         <NavMenu />
       </div>
     </div>
