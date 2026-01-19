4d78926e062c12bf1a804a7b287c0a3d6f7d38d4
muhammadaslamalfarisi <aslamalfarisi0012@gmail.com>
2026-01-19 14:11:58 +0800

    update

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -13,101 +13,122 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
+interface NavMenuProps {
+  isMobile?: boolean;
+}
+
 export function NavMenu() {
   const isMobile = useIsMobile();
 
   return (
-    <NavigationMenu viewport={isMobile}>
-      <NavigationMenuList className="flex-wrap">
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/">Beranda</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="">
-          <NavigationMenuTrigger>Profil</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-50 gap-4">
-              <li>
-                <NavigationMenuLink asChild>
-                  <Link href="#">Profil Sekolah</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">Struktur Organisasi</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">Sarana dan Prasarana</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">Data Peserta Didik</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">PTK</Link>
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="">
-          <NavigationMenuTrigger>Berita</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-50 gap-4">
-              <li>
-                <NavigationMenuLink asChild>
-                  <Link href="#">Pengumuman</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">Undangan</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">Inovasi</Link>
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="">
-          <NavigationMenuTrigger>Program</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-50 gap-4">
-              <li>
-                <NavigationMenuLink asChild>
-                  <Link href="#">SPMB</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">Komunitas Belajar</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">Pembelajaran</Link>
-                </NavigationMenuLink>{" "}
-                <NavigationMenuLink asChild>
-                  <Link href="#">Ekstrakurikuler</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">BOSP</Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">PIP</Link>
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/">Unduhan</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/">JDIH</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/">Kontak</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
+    <NavigationMenu className={isMobile ? "max-w-full justify-start" : ""}>
+      <NavigationMenuList
+        className={isMobile ? "flex-col items-start space-y-2" : "flex-row"}
+      >
+        <NavigationMenuList
+          className={
+            isMobile ? "flex flex-col items-start space-y-2 w-full" : ""
+          }
+        >
+          <NavigationMenuItem>
+            <Link href="/" className={navigationMenuTriggerStyle()}>
+              Beranda
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="">
+            <NavigationMenuTrigger>Profil</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-50 gap-4">
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Profil Sekolah</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Struktur Organisasi</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Sarana dan Prasarana</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Data Peserta Didik</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">PTK</Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="">
+            <NavigationMenuTrigger>Berita</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-50 gap-4">
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Pengumuman</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Undangan</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Inovasi</Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="">
+            <NavigationMenuTrigger>Program</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-50 gap-4">
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">SPMB</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Komunitas Belajar</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Pembelajaran</Link>
+                  </NavigationMenuLink>{" "}
+                  <NavigationMenuLink asChild>
+                    <Link href="#">Ekstrakurikuler</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">BOSP</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">PIP</Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href="/">Unduhan</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href="/">JDIH</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href="/">Kontak</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
       </NavigationMenuList>
     </NavigationMenu>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useState } from "react";
+import { IconMenu2, IconX } from "@tabler/icons-react";
+
 import { IconInnerShadowTop } from "@tabler/icons-react";
 import { NavMenu } from "./nav-menu";
 
@@ -5,21 +10,29 @@ import LogoSekolah from "../../public/logo-sekolah.png";
 import Image from "next/image";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 z-50">
-      <div className="flex items-center justify-between h-32">
-        <div className="flex items-center">
-          <div className="flex items-center gap-3">
-            <Image
-              src={LogoSekolah}
-              alt="Logo SD Negeri 1 Batu Rakit"
-              className="w-10 h-10 object-contain"
-            />
-            <span className="text-xl font-medium">SD Negeri 1 Batu Rakit</span>
-          </div>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex items-center justify-between h-20">
+        <div className="flex items-center space-x-3">
+          <Image src={LogoSekolah} alt="Logo" className="h-10 w-auto" />
+          <span className="text-lg font-bold hidden sm:block">
+            SD Negeri 1 Batu Rakit
+          </span>
         </div>
 
-        <NavMenu />
+        <div className="hidden md:block">
+          <NavMenu />
+        </div>
+
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="p-2 text-gray-600 focus:outline-none"
+          >
+            {isOpen ? <IconX size={28} /> : <IconMenu2 size={28} />}
+          </button>
+        </div>
       </div>
     </div>
   );
